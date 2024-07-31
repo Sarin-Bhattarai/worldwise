3add36befed4b239c7fa94af8d859712a34f8af4
sarin punk <sarin.bhattarai@icp.edu.np>
2024-07-31 10:53:38 +0545

    feat(addons)

--- src/hooks/useUrlPosition.js
+++ src/hooks/useUrlPosition.js
@@ -2,8 +2,8 @@ import { useSearchParams } from "react-router-dom";
 
 export function useUrlPosition() {
   const [searchParams] = useSearchParams();
-  const lat = searchParams?.get("lat");
-  const lng = searchParams?.get("lng");
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
 
   return [lat, lng];
 }
