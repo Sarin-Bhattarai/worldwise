fde69d349fbd6fea4feb812c98e45c3006b16943
sarin punk <sarin.bhattarai@icp.edu.np>
2024-08-01 16:40:33 +0545

    feat(error resloved)

--- src/components/Extras/Map.jsx
+++ src/components/Extras/Map.jsx
@@ -42,36 +42,38 @@ function Map() {
   );
 
   return (
-    <div className={styles.mapContainer}>
-      {!geolocationPosition && (
-        <Button type="position" onClick={getPosition}>
-          {isLoadingPosition ? "Loading..." : "Use your position"}
-        </Button>
-      )}
-      <MapContainer
-        center={mapPosition}
-        zoom={6}
-        scrollWheelZoom={true}
-        className={styles.map}
-      >
-        <TileLayer
-          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
-        />
-        {cities?.map((city) => (
-          <Marker
-            position={[city?.position?.lat, city?.position?.lng]}
-            key={city.id}
-          >
-            <Popup>
-              <span>{city.emoji}</span> <span>{city.cityName}</span>
-            </Popup>
-          </Marker>
-        ))}
-        <ChangeCenter position={mapPosition} />
-        <DetectClick />
-      </MapContainer>
-    </div>
+    <>
+      <div className={styles.mapContainer}>
+        {!geolocationPosition && (
+          <Button type="position" onClick={getPosition}>
+            {isLoadingPosition ? "Loading..." : "Use your position"}
+          </Button>
+        )}
+        <MapContainer
+          center={mapPosition}
+          zoom={6}
+          scrollWheelZoom={true}
+          className={styles.map}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+          />
+          {cities?.map((city) => (
+            <Marker
+              position={[city.position.lat, city.position.lng]}
+              key={city.id}
+            >
+              <Popup>
+                <span>{city.emoji}</span> <span>{city.cityName}</span>
+              </Popup>
+            </Marker>
+          ))}
+          <ChangeCenter position={mapPosition} />
+          <DetectClick />
+        </MapContainer>
+      </div>
+    </>
   );
 }
 
@@ -84,7 +86,7 @@ function ChangeCenter({ position }) {
 function DetectClick() {
   const navigate = useNavigate();
   useMapEvents({
-    click: (e) => navigate(`form?lat=${e.latlng.lat}& lng=${e.latlng.lng}`),
+    click: (e) => navigate(`form?lat=${e.latlng.lat}&lng=${e.latlng.lng}`),
   });
 }
 
