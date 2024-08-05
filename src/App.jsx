9ae24ce0a5e70aa09ac652d8fdd5694aeb423b39
sarin punk <sarin.bhattarai@icp.edu.np>
2024-08-05 16:48:28 +0545

    feat(route protected)

--- src/App.jsx
+++ src/App.jsx
@@ -9,13 +9,14 @@ import Login from "./pages/Login";
 import Product from "./pages/Product";
 import Page404 from "./pages/Page404";
 import Pricing from "./pages/Pricing";
+import ProtectedRoute from "./pages/ProtectedRoute";
 import AppLayout from "./pages/AppLayout";
 import City from "./components/Extras/City";
 import Form from "./components/Extras/Form";
 import CityList from "./components/Extras/CityList";
+import { AuthProvider } from "./contexts/AuthContext";
 import CountryList from "./components/Extras/CountryList";
 import { CitiesProvider } from "./contexts/CitiesContext";
-import { AuthProvider } from "./contexts/AuthContext";
 
 function App() {
   return (
@@ -28,7 +29,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoute>
+                  <AppLayout />
+                </ProtectedRoute>
+              }
+            >
               {/* index route */}
               <Route index element={<Navigate replace to="cities" />} />
               {/* Nested Routes */}
