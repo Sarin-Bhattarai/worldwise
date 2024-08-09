4431bf7569fcf899858de6c792665c2cec76534d
sarin punk <sarin.bhattarai@icp.edu.np>
2024-08-09 12:52:57 +0545

    feat(Lazy loading & Suspense)

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
+import { lazy, Suspense } from "react";
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
 } from "react-router-dom";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import Product from "./pages/Product";
-import Page404 from "./pages/Page404";
-import Pricing from "./pages/Pricing";
 import ProtectedRoute from "./pages/ProtectedRoute";
-import AppLayout from "./pages/AppLayout";
+import { CitiesProvider } from "./contexts/CitiesContext";
+import { AuthProvider } from "./contexts/AuthContext";
+
 import City from "./components/Extras/City";
 import Form from "./components/Extras/Form";
 import CityList from "./components/Extras/CityList";
-import { AuthProvider } from "./contexts/AuthContext";
+import SpinnerFullPage from "./components/Extras/SpinnerFullPage";
 import CountryList from "./components/Extras/CountryList";
-import { CitiesProvider } from "./contexts/CitiesContext";
+//Lazy loading for reducing bundle size.
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Product = lazy(() => import("./pages/Product"));
+const Page404 = lazy(() => import("./pages/Page404"));
+const Pricing = lazy(() => import("./pages/Pricing"));
+const AppLayout = lazy(() => import("./pages/AppLayout"));
 
 function App() {
   return (
@@ -24,30 +28,33 @@ function App() {
     <AuthProvider>
       <CitiesProvider>
         <Router>
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="product" element={<Product />} />
-            <Route path="pricing" element={<Pricing />} />
-            <Route path="login" element={<Login />} />
-            <Route
-              path="app"
-              element={
-                <ProtectedRoute>
-                  <AppLayout />
-                </ProtectedRoute>
-              }
-            >
-              {/* index route */}
-              <Route index element={<Navigate replace to="cities" />} />
-              {/* Nested Routes */}
-              <Route path="cities" element={<CityList />} />
-              <Route path="cities/:id" element={<City />} />
-              <Route path="countries" element={<CountryList />} />
-              <Route path="form" element={<Form />} />
-            </Route>
-            {/* Route for undefined urls */}
-            <Route path="*" element={<Page404 />} />
-          </Routes>
+          {/* <Suspense> lets you display a fallback until its children have finished loading. */}
+          <Suspense fallback={<SpinnerFullPage />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="product" element={<Product />} />
+              <Route path="pricing" element={<Pricing />} />
+              <Route path="login" element={<Login />} />
+              <Route
+                path="app"
+                element={
+                  <ProtectedRoute>
+                    <AppLayout />
+                  </ProtectedRoute>
+                }
+              >
+                {/* index route */}
+                <Route index element={<Navigate replace to="cities" />} />
+                {/* Nested Routes */}
+                <Route path="cities" element={<CityList />} />
+                <Route path="cities/:id" element={<City />} />
+                <Route path="countries" element={<CountryList />} />
+                <Route path="form" element={<Form />} />
+              </Route>
+              {/* Route for undefined urls */}
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </Suspense>
         </Router>
       </CitiesProvider>
     </AuthProvider>
